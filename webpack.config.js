31c56f5fee8e522e7efbf83e6be042e57d3ebe33
Abdurrahman Avcı <aavci@avena.co>
2022-05-04 23:10:06 +0300

    Fix compile

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.s?css$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
