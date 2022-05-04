eeaf3f79964e76de5123f8c65f1da28bed0c3181
Abdurrahman Avcı <avci@retrace.ai>
2022-05-04 11:50:50 +0300

    Updates

---
+++ precache-manifest.d5cb917035a5c0a9e7995f73e081d03a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d1ff532ce33a3f3043c4ae1cd87e9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4e75b25167b5409c2fd",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "2ce90804634af1174e4a",
+    "url": "./static/js/2.34e3612c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.34e3612c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e75b25167b5409c2fd",
+    "url": "./static/js/main.5c30f7f3.chunk.js"
+  },
+  {
+    "revision": "317fccbf0d8203e2196f",
+    "url": "./static/js/runtime-main.80d7eb1e.js"
+  }
+]);
