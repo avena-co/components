56b7393e14482d88ad3da43c1879291168b6afaa
yildirim-baris <74193666+yildirim-baris@users.noreply.github.com>
2022-05-27 09:48:32 +0300

    Add new props to button component

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,9 +3,42 @@ import React from 'react';
 // styles
 import styles from './button.module.scss';
 
-const Button = ({ className, children, ...props }) => {
+const Button = ({ className, children, size, type, ...props }) => {
+    // type => primary(default), secondary, tertiary
+    // size => large, medium(default), small
+    let typeStyle = null;
+    switch (type) {
+        case 'primary':
+            typeStyle = null;
+            break;
+        case 'secondary':
+            typeStyle = styles.secondary;
+            break;
+        case 'tertiary':
+            typeStyle = styles.tertiary;
+            break;
+        default:
+            typeStyle = null;
+            break;
+    }
+
+    let sizeStyle = null;
+    switch (size) {
+        case 'large':
+            sizeStyle = styles.paddingLarge;
+            break;
+        case 'medium':
+            sizeStyle = null;
+            break;
+        case 'small':
+            sizeStyle = styles.paddingSmall;
+            break;
+        default:
+            sizeStyle = null;
+            break;
+    }
     return (
-        <button className={[styles.button, className].filter(Boolean).join(' ')} {...props}>
+        <button className={[styles.button, className, typeStyle, sizeStyle].filter(Boolean).join(' ')} {...props}>
             {children}
         </button>
     );
