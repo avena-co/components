8b0e175606e099b7d1549d349d5411a505a6feb6
yildirim-baris <74193666+yildirim-baris@users.noreply.github.com>
2022-06-07 11:23:22 +0300

    Modify button page to display all types

--- src/pages/button/button.js
+++ src/pages/button/button.js
@@ -14,9 +14,33 @@ const ButtonPage = ({ className, children, ...props }) => {
         <div className={[styles.button, className].join(' ')} {...props}>
             <p>type Props: primary(default), secondary, tertiary</p>
             <p>size Props: large, medium(default), small</p>
-            <Button size='small'>Primary</Button>
-            <Button type='secondary' size='small'>Secondary</Button>
-            <Button type='tertiary' size='small'>Tertiary</Button>
+            <div className={styles.buttonContainer}>
+                <Button size='small'>Primary-Small</Button>
+                <Button size='small' type='secondary' >Secondary-Small</Button>
+                <Button size='small' type='tertiary' >Tertiary-Small</Button>
+                <Button size='small' disabled={true}>Primary-Small-Disabled</Button>
+                <Button size='small' type='secondary' disabled={true}>Secondary-Small-Disabled</Button>
+                <Button size='small' type='tertiary' disabled={true}>Tertiary-Small</Button>
+            </div>
+            <br />
+            <div className={styles.buttonContainer}>
+                <Button >Primary-Default</Button>
+                <Button type='secondary' >Secondary-Default</Button>
+                <Button type='tertiary' >Tertiary-Default</Button>
+                <Button disabled={true}>Primary-Default-Disabled</Button>
+                <Button type='secondary' disabled={true}>Secondary-Default-Disabled</Button>
+                <Button type='tertiary' disabled={true}>Tertiary-Default</Button>
+            </div>
+            <br />
+            <div className={styles.buttonContainer}>
+                <Button size='large'>Primary-Large</Button>
+                <Button size='large' type='secondary' >Secondary-Large</Button>
+                <Button size='large' type='tertiary' >Tertiary-Large</Button>
+                <Button size='large' disabled={true}>Primary-large-Disabled</Button>
+                <Button size='large' type='secondary' disabled={true}>Secondary-large-Disabled</Button>
+                <Button size='large' type='tertiary' disabled={true}>Tertiary-Large</Button>
+            </div>
+            <br />
         </div>
     );
 };
